50bc184283257e7e46edcb2c0210168f63d12518
binamralamsal <binamralamsal@gmail.com>
2023-02-27 20:32:27 +0545

    Removed .env

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
+export type UserRole = "superuser" | "user";
+
 export interface IUser {
   name: string;
   email: string;
   password: string;
+  role: UserRole;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String, default: "user", enum: ["superuser", "user"] },
 });
 
 const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
