058d164ccb0ce670eb1dd7da391978d3d5b9d8fe
binamralamsal <binamralamsal@gmail.com>
2023-03-01 22:39:31 +0545

    Added club model

---
+++ src/models/club.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+import { IUser } from "@/models/user";
+
+export interface IClub {
+  _id: string;
+  name: string;
+  description: string;
+  profilePic: string;
+  members: [
+    {
+      user: IUser;
+      role:
+        | "President"
+        | "Vice President"
+        | "Secretary"
+        | "Active Member"
+        | "General Member"
+        | string;
+    }
+  ];
+  isAvailableForRegistration: boolean;
+  announcements: [
+    {
+      title: string;
+      description: string;
+      photo?: string;
+    }
+  ];
+}
+
+const clubSchema = new mongoose.Schema<IClub>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  profilePic: { type: String, required: true },
+  members: [
+    {
+      user: { ref: "User", required: true },
+      role: { type: "String" },
+    },
+  ],
+  isAvailableForRegistration: { type: Boolean, default: false },
+  announcements: {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    photo: { type: String },
+  },
+});
+
+const Club = mongoose.models.Club || mongoose.model<IClub>("Club", clubSchema);
+export default Club;
