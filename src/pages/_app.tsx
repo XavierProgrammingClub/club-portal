7ba193397f1f39bdaa464506a3b26192c535717b
binamralamsal <binamralamsal@gmail.com>
2023-03-12 23:51:49 +0545

    Added signin page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
 import "@/styles/css-reset.css";
 import "@/styles/globals.css";
+
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
 import "react-toastify/dist/ReactToastify.css";
-import { MantineProvider } from "@mantine/core";
-import { ToastContainer } from "react-toastify";
-
-import { Navbar } from "@/components/Navbar";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,7 +34,8 @@ export default function App({
             <Component {...pageProps} />
           </main>
           <NextNProgress />
-          <ToastContainer />
+          {/*<ToastContainer />*/}
+          <Notifications />
         </MantineProvider>
       </QueryClientProvider>
     </SessionProvider>
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,26 +1,33 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
+import {
+  Paper,
+  Title,
+  Text,
+  Container,
+  TextInput,
+  PasswordInput,
+  Button,
+} from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { showNotification } from "@mantine/notifications";
+import { IconCheck, IconX } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
+import { useState } from "react";
 
-import { Input, Button } from "@/components/ui";
 import { useUser } from "@/hooks/useUser";
 import { queryClient } from "@/pages/_app";
 import { LoginUserCredentialsDTO, loginUserSchema } from "@/validators";
 
-import styles from "./signin.module.css";
-
 const SignIn = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<LoginUserCredentialsDTO>({
-    resolver: zodResolver(loginUserSchema),
-    mode: "all",
+  const form = useForm<LoginUserCredentialsDTO>({
+    validate: zodResolver(loginUserSchema),
+    validateInputOnBlur: true,
+    initialValues: {
+      email: "",
+      password: "",
+    },
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const { data: userData, isLoading } = useUser();
 
@@ -33,75 +40,73 @@ const SignIn = () => {
   }
 
   const handleLogin = async (data: LoginUserCredentialsDTO) => {
+    setIsSubmitting(true);
     const response = await signIn("credentials", {
       redirect: false,
       ...data,
       callbackUrl: "/",
     });
 
-    await queryClient.refetchQueries(["current-user"]);
-    if (response?.ok) return router.push("/");
+    setIsSubmitting(false);
+    if (response?.ok) {
+      await queryClient.refetchQueries(["current-user"]);
+      showNotification({
+        icon: <IconCheck size={16} />,
+        color: "teal",
+        title: "Logged in successfully",
+        message: "Explore the dashboard",
+      });
+      return router.push("/");
+    }
+
     if (response?.error) {
-      toast(response.error, { type: "error" });
+      showNotification({
+        title: "Error Occured while signing in!",
+        message: response.error,
+        icon: <IconX size={16} />,
+        color: "red",
+      });
     }
   };
 
   return (
-    <>
-      <main className={styles.signinWrapper}>
-        <div className={styles.signinContainer}>
-          <div className={`${styles.signinBlock} ${styles.mainBlock}`}>
-            <h1 className={styles.mainBlock__title}>Sign in</h1>
-          </div>
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="md" align="center" mt={5} weight="bold">
+        Invoice App
+      </Text>
 
-          <form
-            className={styles.signinBlock}
-            onSubmit={handleSubmit(handleLogin)}
-          >
-            <Input
-              label="Email"
-              placeholder="sub@xavier.edu.np"
-              autoFocus
-              required
-              {...register("email")}
-              error={errors.email?.message}
-            />
-
-            <Input
-              label="Password"
-              placeholder="********"
-              type="password"
-              required
-              {...register("password")}
-              error={errors.password?.message}
-            />
-
-            <Link className={styles.forgotPassword} href={"/"}>
-              Forgot Password?
-            </Link>
-
-            <Button disabled={isSubmitting}>Sign In</Button>
-          </form>
-        </div>
-      </main>
-    </>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <form onSubmit={form.onSubmit(handleLogin)}>
+          <TextInput
+            label="Email"
+            type="email"
+            placeholder="you@website.com"
+            {...form.getInputProps("email")}
+            error={form.errors.email}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            mt="md"
+            {...form.getInputProps("password")}
+            error={form.errors.password}
+          />
+          <Button fullWidth mt="xl" type="submit" loading={isSubmitting}>
+            Sign in
+          </Button>
+        </form>
+      </Paper>
+    </Container>
   );
 };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const session = await getServerAuthSession(context);
-//
-//   if (session)
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false,
-//       },
-//     };
-//
-//   return {
-//     props: { session },
-//   };
-// };
 
 export default SignIn;
